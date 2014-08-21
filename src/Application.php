74a653e1bdb18fe3b089a98c16e198352f3a9aa3
Thomas ZILLIOX <thomas@zilliox.me>
2014-08-21 19:07:59 +0200

    Add index template

--- src/Application.php
+++ src/Application.php
@@ -6,14 +6,25 @@ define('ROOT_PATH', dirname(__DIR__));
 
 require_once __DIR__ . '/Autoloader.php';
 
-use ProjectHub\Entity\TimeLineEntity;
+use ProjectHub\Entity\ProjectEntity;
 
 class Application
 {
 
+
+    /* ATTRIBUTES
+     *************************************************************************/
+    public $publicBaseUri = '/public';
+
+
+    
+    /* PUBLIC METHODS
+     *************************************************************************/
     public function index()
     {
-        var_dump((new TimeLineEntity)->fetchAll());
+        $project = (new ProjectEntity)->fetchById('sample');
+        $application = $this;
+        require_once(ROOT_PATH.'/template/index.php');
     }
 
 }
