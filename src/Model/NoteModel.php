dff8e4b8f006e4d4ec77e0a80bb7cccdbe519cbc
Thomas ZILLIOX <thomas@zilliox.me>
2014-08-22 11:52:11 +0200

    Add date on Note & Project Model

--- src/Model/NoteModel.php
+++ src/Model/NoteModel.php
@@ -9,8 +9,22 @@ class NoteModel extends \ProjectHub\Model
     /* ATTRIBUTES
      *************************************************************************/
     public $schema =array(
+        'date' => '',
         'stamp' => '',
         'content' => '',
         'links' => array()
     );
+
+
+
+    /* INITIALIZATION
+     *************************************************************************/
+    protected function initialization()
+    {
+        $time = strtotime($this->stamp);
+        if ($time !== false) {
+            $this->date = new \DateTime($this->stamp);
+            $this->stamp = $this->date->format("F j, Y");
+        }
+    }
 }
--- src/Model/ProjectModel.php
+++ src/Model/ProjectModel.php
@@ -11,6 +11,8 @@ class ProjectModel extends \ProjectHub\Model
     /* ATTRIBUTES
      *************************************************************************/
     public $schema =array(
+        'date' => '',
+        'stamp' => '',
         'name' => '',
         'timeline' => array()
     );
@@ -21,14 +23,34 @@ class ProjectModel extends \ProjectHub\Model
      *************************************************************************/
     protected function initialization()
     {
-        if ($this->timeline) {
-            $timeline = array();
+        $this->initializeTimeline();
+        $this->initializeStamp();
+    }
+    
+    protected function initializeTimeline()
+    {
+        $timeline = array();
+        if (is_array($this->timeline)) {
             foreach ($this->timeline as $index => $noteData) {
                 $noteData['id'] = $index;
                 $note = (new NoteModel)->initByData($noteData);
                 $timeline[] = $note;
             }
-            $this->timeline = $timeline;
+        }
+        $this->timeline = $timeline;
+    }
+
+    protected function initializeStamp()
+    {
+        $dateList = array();
+        foreach ($this->timeline as $note) {
+            if (is_object($note->date)) {
+                $dateList[] = $note->date;
+            }
+        }
+        if (count($dateList)) {
+            $this->date = max($dateList);
+            $this->stamp = $this->date->format("F j, Y");
         }
     }
 }
